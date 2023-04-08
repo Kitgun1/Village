c7cec5d4658d13ee29973c9a6966a7a25d434a63
Kimicu <kitgunplay@gmail.com>
2023-04-08 11:54:39 +0300

    Написал Pool объектов

---
+++ Assets/_Project/Scripts/KimicuUtilities/Pool/KiPool.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace _Project.Scripts.KimicuUtilities.Pool
+{
+    public static class KiPool
+    {
+        private static readonly List<Pool> Pools = new List<Pool>();
+
+        public static Action<GameObject> OnSpawn;
+        public static Action<GameObject> OnDespawn;
+
+        public static T Spawn<T>(T component, Vector3 position = default(Vector3),
+            Quaternion rotation = default(Quaternion)) where T : Component
+        {
+            return DefaultSpawn(component.gameObject, position, rotation, null).GetComponent<T>();
+        }
+
+        public static T Spawn<T>(T component, Transform parent, Vector3 position = default(Vector3),
+            Quaternion rotation = default(Quaternion)) where T : Component
+        {
+            return DefaultSpawn(component.gameObject, position, rotation, parent).GetComponent<T>();
+        }
+
+        public static GameObject Spawn(GameObject spawnObject, Vector3 position = default(Vector3),
+            Quaternion rotation = default(Quaternion))
+        {
+            return DefaultSpawn(spawnObject, position, rotation, null);
+        }
+
+        public static GameObject Spawn(GameObject spawnObject, Transform parent, Vector3 position = default(Vector3),
+            Quaternion rotation = default(Quaternion))
+        {
+            return DefaultSpawn(spawnObject, position, rotation, parent);
+        }
+
+        public static void Despawn(Component despawnObject, float delay)
+        {
+            DefaultDespawn(despawnObject.gameObject, delay);
+        }
+
+        public static void Despawn(GameObject despawnObject, float delay)
+        {
+            DefaultDespawn(despawnObject, delay);
+        }
+
+        private static Pool GetPool(GameObject prefab)
+        {
+            foreach (Pool pool in Pools)
+            {
+                if (pool.Prefab == prefab)
+                {
+                    return pool;
+                }
+            }
+
+            return CreatePool(prefab);
+        }
+
+        private static Pool CreatePool(GameObject prefab)
+        {
+            GameObject poolParent = new GameObject($"[KiPool] {prefab.name}");
+            Pool pool = poolParent.AddComponent<Pool>();
+
+            pool.Init(prefab, poolParent.transform);
+            Pools.Add(pool);
+
+            return pool;
+        }
+
+        private static GameObject DefaultSpawn(GameObject prefab, Vector3 position, Quaternion rotation,
+            Transform parent)
+        {
+            Pool pool = GetPool(prefab);
+            GameObject poolItem = pool.GetFreeGameobject();
+
+            poolItem.SetActive(true);
+
+            poolItem.transform.SetParent(parent, false);
+            poolItem.transform.SetPositionAndRotation(position, rotation);
+
+            OnSpawn?.Invoke(poolItem);
+
+            return poolItem;
+        }
+
+        private static async void DefaultDespawn(GameObject despawnObject, float delay = 0F)
+        {
+            if (delay > 0F)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(delay));
+                if (despawnObject == null)
+                {
+                    return;
+                }
+            }
+
+            despawnObject.SetActive(false);
+            OnDespawn?.Invoke(despawnObject);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/KimicuUtilities/Pool/Pool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Project.Scripts.KimicuUtilities.Pool
+{
+    [DisallowMultipleComponent]
+    public class Pool : MonoBehaviour
+    {
+        public GameObject Prefab { get; private set; }
+        public Transform Parent { get; private set; }
+
+        private readonly List<GameObject> _pool = new List<GameObject>();
+
+        private bool _isInit;
+
+        public void Init(GameObject prefab, Transform parent)
+        {
+            if (_isInit) return;
+
+            Prefab = prefab;
+            Parent = parent;
+            _isInit = true;
+        }
+
+        public GameObject GetFreeGameobject()
+        {
+            if (_pool.Count <= 0) return Instantiate();
+            GameObject item = _pool[0];
+            _pool.RemoveAt(0);
+            return item;
+        }
+
+        private GameObject Instantiate()
+        {
+            if (Prefab == null && Parent == null) throw new ArgumentException();
+
+            GameObject item = Instantiate(Prefab, Parent);
+            item.SetActive(false);
+            _pool.Add(item);
+            return item;
+        }
+    }
+}
